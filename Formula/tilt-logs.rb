59dc7d3ca5effd81c3988046497ed72ae96f07a1
Chris Masterton <crmasterton@gmail.com>
2025-11-12 23:07:40 -0800

    Fix SHA256 replacement logic in workflow and correct Intel checksum

--- Formula/tilt-logs.rb
+++ Formula/tilt-logs.rb
@@ -10,7 +10,7 @@ class TiltLogs < Formula
     sha256 "6e83979c64fd610aaafbc9b00c7ff3a9bc6748c0de7d84910e1123bac075e474"
   elsif OS.mac? && Hardware::CPU.intel?
     url "https://github.com/ChrisMasterton/tilt-log-cli/releases/download/v0.1.6/tilt-logs-x86_64-apple-darwin.tar.gz"
-    sha256 "6e83979c64fd610aaafbc9b00c7ff3a9bc6748c0de7d84910e1123bac075e474"
+    sha256 "4c9a0c9ed666342adc1bcc71673dd900a8bc8fc245c529f67ce683cadd01e061"
   else
     odie "Unsupported platform"
   end
